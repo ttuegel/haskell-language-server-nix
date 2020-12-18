f9ef577b37ccfd85820adbc2d18831ff1791d640
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-17 21:13:06 -0600

    Rename default attributes

--- default.nix
+++ default.nix
@@ -6,8 +6,8 @@ let
 in
 
 {
-  "ghc-8.8.3" = get "ghc-8.8.3";
-  "ghc-8.8.4" = get "ghc-8.8.4";
-  "ghc-8.10.1" = get "ghc-8.10.1";
-  "ghc-8.10.2" = get "ghc-8.10.2";
+  ghc-8_8_3 = get "ghc-8.8.3";
+  ghc-8_8_4 = get "ghc-8.8.4";
+  ghc-8_10_1 = get "ghc-8.10.1";
+  ghc-8_10_2 = get "ghc-8.10.2";
 }
